88f6397606e61bd7b676fc923ececf3237793c09
Eslam Hesham <esl2mhesham@gmail.cm>
2020-06-27 18:44:06 +0200

    building sandwich

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!',
-      secMessage: `I'm Out there!`
-    }
-  })
+  el: "#app",
+  data: {
+    message: "Hello Vue!",
+    secMessage: `I'm Out there!`,
+    seen: true,
+  },
+});
